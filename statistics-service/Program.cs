9de52fe5e7a0e57c37ccd847843ed62863cc3b9a
AljazRant <aljaz.rant@gmail.com>
2026-01-18 15:45:55 +0100

    Added CORS to statistics-service for localhost:4200

--- statistics-service/Program.cs
+++ statistics-service/Program.cs
@@ -66,6 +66,17 @@ builder.Services.AddSwaggerGen(c =>
     });
 });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowLocalhost4200", policy =>
+    {
+        policy
+            .WithOrigins("http://localhost:4200")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 app.UseSwagger();
